1e09416b98567f48f12b18df8bdfe11034824568
IngridAMorales <68444503+IngridAMorales@users.noreply.github.com>
2021-11-12 16:43:04 -0800

    Create div.hpp

---
+++ div.hpp
@@ -0,0 +1,25 @@
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+
+#include "base.hpp"
+#include <stdexcept>
+
+class Div : public Base {
+	private:
+	  Base* left;
+	  Base* right;
+
+	public:
+	  Div(Base* op1, Base* op2) : Base(){
+		left = op1;
+		right = op2;
+}
+	virtual double evaluate(){
+		 return this->left->evaluate() / this->right->evaluate(); 
+}
+	   virtual std::string stringify() {
+		return "("+ this->left->stringify()+ " / "+ this->right->stringify()+ ")"; 
+	}
+};
+
+#endif 
