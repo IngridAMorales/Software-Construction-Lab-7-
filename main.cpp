d57d988cf68caaefde0f0cb42ffd6e87366ed675
Viviane Fernando <viviane9533@gmail.com>
2021-11-18 12:32:15 -0800

    Main.cpp testing

--- main.cpp
+++ main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 
 #include "base.hpp"
-#include "mult.hpp" 
-#include "div.hpp"
-#include "op.hpp"
-#include "power.hpp"
-#include "sub.hpp"
-#include "add.hpp"
+#include "parse.hpp"
 
-int main() {
+int main(int argc, char** argv) {
     // This is a very basic main, and being able to correctly execute this main
     // does not constitute a completed lab. Make sure you write unit tests for
     // all the classes that you create (and can be instantiated) in this lab
-    Base* three = new Op(3);
-    Base* seven = new Op(7);
-    Base* four = new Op(4);
-    Base* two = new Op(2);
-    Base* mult = new Mult(seven, four);
-    Base* add = new Add(three, mult);
-    Base* minus = new Sub(add, two);
-
-    std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
+    Parse* test;
+     std::cout << argc;
+     Base* test1 = test->parse(argv+1,argc);
+    std::cout << test1->stringify() << std::endl;
     return 0;
 }
