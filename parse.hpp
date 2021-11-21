b475732532672b5a23d4fad68d1cd900ef1995c6
IngridAMorales <ingridmorales1016@gmail.com>
2021-11-21 15:24:41 -0800

    Finished writing the rest of the operators

--- parse.hpp
+++ parse.hpp
@@ -5,9 +5,11 @@
 #include <string>
 #include "op.hpp"
 #include "add.hpp"
-
-
+#include "sub.hpp"
+#include "mult.hpp"
+#include "power.hpp"
 class Parse {
+ 
 public:
    
    Base* parse(char** input, int length){
@@ -15,21 +17,45 @@ public:
 	if ( length % 2 ==0){
 	  return nullptr;
 	}
-	else {
+	else{
 	try{
-   	Base*  output = new Op(std::stod(input[0]));
+   	 Base* output = new Op(std::stod(input[0]));
 	}
 	catch(std::exception e){
   	  return nullptr;
 	}
 	}
-	if (length > 2 && std::string(input[1]) == "+"){
-	  Base* output = new Op(std::stod(input[0]));
-   	  Base* second= new Op(std::stod(input[2]));
-   	  Base* add = new Add(output,second);
-   	  return add;
+	Base* output = new Op(std::stod(input[0]));
+	for( int i = 1; i< length; ++i) {
+	  if (std::string(input[i]) == "+"){
+   	  	Base* second= new Op(std::stod(input[i+1]));
+   	  	Base* add = new Add(output,second);
+   	  	output = add;
+	  }
+          else if(std::string(input[i]) == "-"){
+	        Base* second= new Op(std::stod(input[i+1]));
+                Base* sub = new Sub(output,second);
+                output = sub;		
+
+	  } 
+	  else if(std::string(input[i]) == "\*"){
+                Base* second= new Op(std::stod(input[i+1]));
+                Base* mult = new Mult(output,second);
+                output = mult;
+
+          }
+	  else if(std::string(input[i]) == "/"){
+                Base* second= new Op(std::stod(input[i+1]));
+                Base* div = new Div(output,second);
+                output = div;
+	  }
+	  else if(std::string(input[i]) == "\*\*"){
+                Base* second= new Op(std::stod(input[i+1]));
+                Base* pow = new Pow(output,second);
+                output = pow;
+          }
 	}
-	
+	return output; 	
    }
 };
 
